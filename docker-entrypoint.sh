f9b98b6fba4ab4c870abd2e37ffcfd15271a1601
grtmrcl <grtmrcl@gmail.com>
2026-04-10 15:44:55 +0900

    BASIC認証を調整

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,11 @@ set -e
 
 if [ -n "$BASIC_AUTH_USER" ] && [ -n "$BASIC_AUTH_PASSWORD" ]; then
     htpasswd -bc /etc/nginx/.htpasswd "$BASIC_AUTH_USER" "$BASIC_AUTH_PASSWORD"
+    sed -i 's/__AUTH_BASIC_REALM__/Restricted/' /etc/nginx/conf.d/default.conf
 else
-    # 認証情報が未設定の場合は空ファイルを置いてnginxが起動できるようにする
+    # 認証情報が未設定の場合は認証を無効化する
     touch /etc/nginx/.htpasswd
+    sed -i 's/__AUTH_BASIC_REALM__/off/' /etc/nginx/conf.d/default.conf
 fi
 
 exec nginx -g "daemon off;"
